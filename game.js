b55fac32d78dfff047612e8388b0334cc2f5eb1a
nneverson <neelyneverson@yahoo.com>
2020-10-11 16:06:49 -0400

    more-js

--- game.js
+++ game.js
@@ -2,7 +2,11 @@ const button1 = document.getElementById('button1');
 const heading = document.getElementById('heading');
 
 button1.addEventListener('click', function () {
-	document.body.style.backgroundImage = "url('../Finding Memo frame 1.png')";
+	document.body.style.backgroundImage =
+		"url('https://i.imgur.com/EVduQKU.jpg')";
 	button1.style.display = 'none';
 	heading.style.display = 'none';
+	document.querySelector('#question1').style.display = 'inline-block';
 });
+
+
