e7e42f9d2417c29ef924c87c3b8ea72386f06571
nneverson <neelyneverson@yahoo.com>
2020-10-12 06:28:42 -0400

    score

--- game.js
+++ game.js
@@ -8,3 +8,34 @@ button1.addEventListener('click', function () {
 	heading.style.display = 'none';
 	document.querySelector('#question1').style.display = 'inline-block';
 });
+const question 1 = document.getElementById("question1")
+const question 2 = document.getElementById("question2")
+const question 3 = document.getElementById("question3")
+const question 4 = document.getElementById("question4")
+const question 5 = document.getElementById("question5")
+const question 6 = document.getElementById("question6")
+const question 7 = document.getElementById("question7")
+const question 8 = document.getElementById("question8")
+const question 9 = document.getElementById("question9")
+const question 10 = document.getElementById("question10")
+ 
+let rightChoices = [
+	'Sea anemones',
+	'Fish are friends not food',
+	'Mucus on Marlon, protected him',
+	'False',
+	'A School',
+	'Mother Cichild fish',
+	'Royal Blue Tang',
+	'Bioluminescent bacteria',
+	'False',
+	'All of the above',
+];
+
+function correctAnswer (rightChoices){
+	if (rightchoices) {
+		rightChoices[0] != 
+	} else {
+		
+	}
+}
