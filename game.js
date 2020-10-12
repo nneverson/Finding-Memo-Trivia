15dbeb61392e339da6c2fc2582cf0b1fd8d0bfca
nneverson <neelyneverson@yahoo.com>
2020-10-12 07:09:16 -0400

    js-functions

--- game.js
+++ game.js
@@ -1,23 +1,17 @@
 const button1 = document.getElementById('button1');
 const heading = document.getElementById('heading');
+const div = document.getElementsid("hiddenDiv")
+
+let score = 100;
+const scoreDiv = document.getElementById("user-score")
 
 button1.addEventListener('click', function () {
 	document.body.style.backgroundImage =
 		"url('https://i.imgur.com/EVduQKU.jpg')";
 	button1.style.display = 'none';
-	heading.style.display = 'none';
+	div.style.display
 	document.querySelector('#question1').style.display = 'inline-block';
 });
-const question 1 = document.getElementById("question1")
-const question 2 = document.getElementById("question2")
-const question 3 = document.getElementById("question3")
-const question 4 = document.getElementById("question4")
-const question 5 = document.getElementById("question5")
-const question 6 = document.getElementById("question6")
-const question 7 = document.getElementById("question7")
-const question 8 = document.getElementById("question8")
-const question 9 = document.getElementById("question9")
-const question 10 = document.getElementById("question10")
  
 let rightChoices = [
 	'Sea anemones',
